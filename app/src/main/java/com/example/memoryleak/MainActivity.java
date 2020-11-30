d1c5022d078977b53b67638d86f2f4c9482abd21
asifzafar_pc <asifzafar00>
2020-11-30 20:24:10 +0530

    initial chnages

---
+++ app/src/main/java/com/example/memoryleak/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.memoryleak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.SystemClock;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        new DownloadTask(this).start();
+
+    }
+
+    private class DownloadTask extends Thread {
+
+        Activity activity;
+
+        public DownloadTask(Activity activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        public void run() {
+            SystemClock.sleep(2000 * 10);
+        }
+    }
+
+}
