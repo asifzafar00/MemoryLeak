633851557cd4275e0a2a81360a72fc337b33e11d
asifzafa00 <asifzafar00@gmail.com>
2020-11-30 23:45:35 +0530

    changes done after head reset

--- app/src/main/java/com/example/memoryleak/MainActivity.java
+++ app/src/main/java/com/example/memoryleak/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         
         new DownloadTask(this).start();
+        // changes done
+
 
     }
 
