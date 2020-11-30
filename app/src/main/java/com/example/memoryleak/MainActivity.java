6efc2bdb20a4458aaf57ed93b5f6bad96ae41068
asifzafa00 <asifzafar00@gmail.com>
2020-11-30 23:49:59 +0530

    commenrs

--- app/src/main/java/com/example/memoryleak/MainActivity.java
+++ app/src/main/java/com/example/memoryleak/MainActivity.java
@@ -13,8 +13,11 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         new DownloadTask(this).start();
+        // changes done
+        // changes done
+        // changes done
         // changes done
 
 
